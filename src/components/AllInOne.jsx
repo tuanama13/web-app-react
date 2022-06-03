4e5415a0088e89d351b3ae4e56f3c18ee47dec19
tuanama13 <tuanama7@gmail.com>
2022-06-03 23:28:31 +0700

    add more pages

---
+++ src/components/AllInOne.jsx
@@ -0,0 +1,92 @@
+import { CheckIcon } from '@heroicons/react/solid'
+import React from 'react'
+
+const AllInOne = () => {
+  return (
+    <div className='w-full mt-24'>
+      <div className='max-w-[1240px] mx-auto px-2'>
+        <h2 className='text-5xl font-bold text-center'>All-In-One Platform</h2>
+        <p className='text-2xl py-8 text-gray-500 text-center'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In quia atque nobis sapiente, suscipit ullam dolores natus quos deserunt ea. Ipsam amet laborum provident possimus officia excepturi iure nulla ut.
+        </p>
+
+        <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Notification</h3>
+              <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor reiciendis maiores beatae vero ipsa porro cupiditate cumque soluta architecto eveniet et, iure sed quaerat non itaque eum. Animi, quam.</p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Notification</h3>
+              <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor reiciendis maiores beatae vero ipsa porro cupiditate cumque soluta architecto eveniet et, iure sed quaerat non itaque eum. Animi, quam.</p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Notification</h3>
+              <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor reiciendis maiores beatae vero ipsa porro cupiditate cumque soluta architecto eveniet et, iure sed quaerat non itaque eum. Animi, quam.</p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Notification</h3>
+              <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor reiciendis maiores beatae vero ipsa porro cupiditate cumque soluta architecto eveniet et, iure sed quaerat non itaque eum. Animi, quam.</p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Notification</h3>
+              <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor reiciendis maiores beatae vero ipsa porro cupiditate cumque soluta architecto eveniet et, iure sed quaerat non itaque eum. Animi, quam.</p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Notification</h3>
+              <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor reiciendis maiores beatae vero ipsa porro cupiditate cumque soluta architecto eveniet et, iure sed quaerat non itaque eum. Animi, quam.</p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Notification</h3>
+              <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor reiciendis maiores beatae vero ipsa porro cupiditate cumque soluta architecto eveniet et, iure sed quaerat non itaque eum. Animi, quam.</p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Notification</h3>
+              <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor reiciendis maiores beatae vero ipsa porro cupiditate cumque soluta architecto eveniet et, iure sed quaerat non itaque eum. Animi, quam.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AllInOne
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {CloudUploadIcon,UploadIcon,PaperAirplaneIcon,ServerIcon} from '@heroicons/react/solid';
+import {CloudUploadIcon,PaperAirplaneIcon,ServerIcon, DatabaseIcon} from '@heroicons/react/solid';
 import bgImg from "../assets/cyber-bg.png";
 
 
@@ -9,9 +9,21 @@ const Hero = () => {
       <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
         <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
           <p className='text-2xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-          <h1 className='py-3 text-5xl md:text-7xl'>Cloud Management</h1>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-          <button>Get Started</button>
+          <h1 className='py-3 text-5xl md:text-7xl font-bold'>Cloud Management</h1>
+          <p className='text-2xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          <button className='py-3 px-6 sm:w-[60%] my-4'>Get Started</button>
+        </div>
+        <div>
+          <img className='w-full' src={bgImg} alt="/" />
+        </div>
+        <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-[-5%] mx-1 md:left-1/2 md:-translate-x-1/2 bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl'>
+          <p>Data Service</p>
+          <div className='flex justify-between flex-wrap px-4'>
+            <p className='flex px-4 py-2'><CloudUploadIcon className='h-6 text-indigo-600'/> App Security</p>
+            <p className='flex px-4 py-2'><DatabaseIcon className='h-6 text-indigo-600'/> Dashboard Design</p>
+            <p className='flex px-4 py-2'><ServerIcon className='h-6 text-indigo-600'/> Cloud Data</p>
+            <p className='flex px-4 py-2'><PaperAirplaneIcon className='h-6 text-indigo-600'/> API</p>
+          </div>
         </div>
       </div>
     </div>
